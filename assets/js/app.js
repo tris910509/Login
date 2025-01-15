a32966dd37c9f61e9a83e94ad53b224801261e12
tris910509 <63364142+tris910509@users.noreply.github.com>
2025-01-15 09:37:02 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+
 //Fungsi JavaScript untuk Login
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
